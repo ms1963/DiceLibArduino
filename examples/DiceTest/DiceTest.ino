4aa28ffd7b73586d7fbf027ddf06e7d0c7e61f65
Michael Stal <michael.stal@gmail.com>
2016-07-13 01:49:10 +0200

    Created example for Arduino

---
+++ examples/DiceTest/DiceTest.ino
@@ -0,0 +1,15 @@
+#define DEBUG
+#include "Dice.h"
+
+// Dice with LEDs on digital Pins 9, 10, 11
+Dice myDice(9,10,11);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  myDice.roll();  // roll the dice
+  delay(2000);
+} 
